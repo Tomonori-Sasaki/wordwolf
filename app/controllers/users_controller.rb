0c93675fca4360c172e654ff91971e2e161cd75f
Tomonori-Sasaki <sasaki-56@ruri.waseda.jp>
2017-09-06 23:16:25 +0900

    player画面まで

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,18 @@
 class UsersController < ApplicationController
   def new
   end
+
+  def create
+    if params[:num].to_i >= 3
+     redirect_to("/users/player/#{params[:num]}")
+    else
+      flash[:notice] = "お前#{params[:num]}人しか友達いないんか！？"
+      render("users/new")
+    end
+  end
+
+  def player
+    @num = params[:num]
+  end
+
 end
