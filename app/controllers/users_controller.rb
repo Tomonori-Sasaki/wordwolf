7785ecbdaffc7c1fac147e433ba9cc0d09b3401d
Tomonori-Sasaki <sasaki-56@ruri.waseda.jp>
2017-09-08 07:29:49 +0900

    route no

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,17 @@
 class UsersController < ApplicationController
+
+  def index
+  end
+
   def new
     @user = User.new
   end
 
   def create
-    if params[:num].to_i >= 3
-     redirect_to("/users/player/#{params[:num]}")
-    else
-      flash[:notice] = "お前#{params[:num]}人しか友達いないんか！？"
-      render("users/new")
-    end
+    redirect_to(users_path)
   end
 
-  def player
-    @num = params[:num]
+  def setting
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  get 'users/create'
-  get 'users/player/:num' => 'users#player'
-  get 'users/create_player'
   get '/' => 'home#top'
+  resources :users
+  get 'users/setting'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
