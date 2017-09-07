1f61678b3701d603211619710e8654a89f2f9db4
yoda <yoda@yoda.local>
2017-09-07 17:49:01 +0900

    レイアウトの調整

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
@@ -14,5 +15,6 @@ class UsersController < ApplicationController
   def player
     @num = params[:num]
   end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   get 'users/new'
-  post 'users/create' => 'users#create'
+  get 'users/create'
   get 'users/player/:num' => 'users#player'
+  get 'users/create_player'
   get '/' => 'home#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
